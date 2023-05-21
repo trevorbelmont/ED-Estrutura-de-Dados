8fb92f04572f1ccc91e6360b928f95d3bd89bea3
trevorbelmont <belmont.programmer@gmail.com>
2023-05-21 00:25:21 -0300

    !Viabiliza implict cast  Node<tipoA> = node<tipoB>
    Struct Node<Tipo>:
        Viabiliza , via sobrecarregamento de operador = (algo a se rever),a conversão implícita automática mais básica das chaves de nodes de tipos diferentes dentro do próprio struct genérico node<Tipo>.
    Class List<Tipo>:
        Sobrecarrega o operador = para nodes de tipo diferentes, desde que a conversão Node<Tipo>(Node<TipoRight>), uma vez que essa é usada.

--- TP7/include/lista.hpp
+++ TP7/include/lista.hpp
@@ -15,20 +15,51 @@ struct Node {
   Tipo key;
   Node<Tipo> *prev = nullptr;
   Node<Tipo> *next = nullptr;
+
+  // !! Sobrecarrega o operador = para Nodos A chave é atribuida e ponteiros do Nodo não são tocados.
+  // Usa os casts padrões para tentar atribuir. Não altera a variável se falhar. ¬ Fora do padrão ISO C++.
+  template <typename TipoRight>
+  Node<Tipo> operator=(Node<TipoRight> right) const {
+    Node<Tipo> converted = *this;
+    try {
+      converted.key = Tipo(right.key);
+      return converted;
+    } catch (...) {
+      cout << " !! Cast não definido! A Variável não foi alterada." << endl;
+    }
+    return *this;
+  }
+
+  // Retorna uma versão do nodo atual , nodo<Tipo>, convertido para o nodo do tipo requisitado , Nodo<ConversionType>.
+  // !Atenção: o Nodo<ConversionTipe> retornado não possui qualquer ponteiro inicializado.
+  // Pela natureza diferente (e a não implementação de hierarquia) nodos de tipos diferentes não podem compartilhar ponteiros para nodos.
+  template <typename ConversionType>
+  operator Node<ConversionType>() const {
+    Node<ConversionType> conversion;
+    conversion.key = TipoRight(this->key);
+    return conversion;
+  }
 };
 
 // Lista encadeada de tipo genérico
 template <typename Tipo>
 class Lista {
  private:
-  Node<Tipo> *first;  // Ponteiro Node<Tipo> para oo início da lista (primiera posição);
-  Node<Tipo> *last;   // Ponteiro Node<Tipo> para a próxima posição OCUPADA da lista.
-
-  int size_;  // Variável privada que designa o tamanho do lista;
+  Node<Tipo> *first_;  // Ponteiro Node<Tipo> para oo início da lista (primiera posição);
+  Node<Tipo> *last_;   // Ponteiro Node<Tipo> para a próxima posição OCUPADA da lista.
+  int size_;           // Variável privada que designa o tamanho do lista;
 
  public:
+  // Sobrecarrega o operador = para nodes de tipos diferentes (desde que o cast Node<tipo>(Node<TipoRight>)) tenha sido implementada.
+  template <typename TipoRight>
+  Node<Tipo> operator=(Node<TipoRight> right) {  // queria saber se rodava
+    Node<Tipo> converted;
+    converted.key = Tipo(right.key);
+    return converted;  // chega a rodar
+  }
+
   Lista() {
-    first = last = new Node<Tipo>;
+    first_ = last_ = new Node<Tipo>;
     size_ = 0;
   }
 
@@ -38,11 +69,11 @@ class Lista {
     aux->key = t;
 
     if (size_ == 0) {
-      last = first = aux;
+      last_ = first_ = aux;
     } else {
-      aux->prev = last;
-      last->next = aux;
-      last = aux;
+      aux->prev = last_;
+      last_->next = aux;
+      last_ = aux;
     }
     size_++;
   }
@@ -52,21 +83,13 @@ class Lista {
     Node<Tipo> *aux = new Node<Tipo>;
     aux->key = t;
     if (size_ == 0) {
-      first = last = aux;
+      first_ = last_ = aux;
     } else {
-      aux->next = first;
-      first->prev = aux;
-      first = aux;
+      aux->next = first_;
+      first_->prev = aux;
+      first_ = aux;
     }
     size_++;
-  }
-  // Retorna um ponteiro para a posição especificada. Retorna nullptr caso falhar.
-  Node<Tipo> *get(int pos) {
-    int i = 0;
-    Node<Tipo> *it = first;
-    for (int i = 0; i < pos; i++) {  // percorre a lista só até a posição i
-      it = it->next;
-    }
   }
   // Insere antes da posição especificada pelo ponteiro do segundo argumento. Retorna false, caso falhe.
   bool insert(Tipo t, Node<Tipo> *pos) {
@@ -88,7 +111,7 @@ class Lista {
 
   // Meramente apaga o elemento de índice especifico, pos, porém sem retorná-lo. Retorna falso caso falhar em remover;
   bool erase(int pos) {
-    if (pos >= size) {
+    if (pos >= size_) {
       return false;
     } else {
       Node<Tipo> *del = get(pos);   // O Node<Tipo> a ser deletado.
@@ -100,36 +123,46 @@ class Lista {
   }
 
   // Meramente apaga o elemento indicado pelo pointer específico sem retorná-lo. Retorna falso caso falhe.
-  // bool erase(Node<Tipo> *ptr);
+  bool erase(Node<Tipo> *del) {
+    if (del == nullptr) {
+      return false;
+    } else {
+      del->prev->next = del->next;  // Emenda o ponteiro next do anterior com o do próximo
+      del->next->prev = del->prev;  // Emenda o ponteiro prev do próximo com o do anterior
+      return true;
+    }
+  }
 
   // Remove o elemento apontado pelo ponteiro e retorna um ponteiro pra próxima posição válida.
   //*Node<Tipo> remove(Node<Tipo> *ptr);
 
   // Remove e retorna uma cópia do elemento da posição, pos, do índice específico;
-  Tipo remove(int pos);
+  Tipo remove(int pos) {}
 
-  // Retira e retorna o último elemento. 
+  // Retira e retorna o último elemento.
   Tipo pop_back() {
     if (size_ <= 0) {
       Tipo nulinho;
-      return nulinho; // ¬Devolve lixo se falhar
+      return nulinho;  // ¬Devolve lixo se falhar
     }
-    Node<Tipo> popped = *last;  // O node a ser retirado recebe uma cópia do nodo apontado por last
-    last = last->prev;          // O ponteiro privado last passa ser o penúltimo (que ainda aponta o antigo último)
-    delete last->next;          // Desaloca o antigo último da lista
-    size_--;                    // ¬ não desalocou memória.
+    Node<Tipo> popped = *last_;  // O node a ser retirado recebe uma cópia do nodo apontado por last_
+    last_ = last_->prev;
+    last_->next = nullptr;  // O ponteiro privado last_ passa ser o penúltimo (que ainda aponta o antigo último)
+    delete last_->next;     // Desaloca o antigo último da lista
+    size_--;                // ¬ não desalocou memória.
     return popped.key;
   }
 
-  // Retira e retorna o primeiro elemento. 
+  // Retira e retorna o primeiro elemento.
   Tipo pop_front() {
     if (size_ <= 0) {
       Tipo t;
-      return t; // ¬Devolve lixo se falhar
+      return t;  // ¬Devolve lixo se falhar
     }
-    Node<Tipo> popped = *(first);  // Faz cópia de first.
-    delete first;                  // Desaloca o atual first (na front da lista)
-    first = (popped.next);         // Atualiza o novo first.
+    Node<Tipo> popped = *(first_);  // Faz cópia de first_.
+    delete first_;                  // Desaloca o atual first_ (na front da lista)
+    first_ = (popped.next);         // Atualiza o novo first_.
+    first_->prev = nullptr;         // Atualiza o ponteiro do first para nullptr;
     size_--;
     // ¬¬ Acho que é porque node poppoed está alocado estáticamente
 
@@ -145,21 +178,48 @@ class Lista {
     return size_;
   }
 
-  // Retorna o nodo da posição, pos, especificada. Retorna um nodo totalmente nulo, se falhar
-  Node<Tipo> at(int pos) {
-    if (pos > size) {
-      return nullptr;
+  // Lista as entradas da lista separadas pela string separadora especificada.
+  /// Caso não especificado um blank space é utilizado.
+  void list(string sep = "  ") {
+    if (size_ <= 0) {
+      cout << "!Empty List" << endl;
+      return;
     }
-    int i = 0;
-    Node<Tipo> aux = first;
-    while (aux != end()) {
-      aux = aux->next;
-    }
+    Node<Tipo> *it = first_;
+    do {
+      cout << it->key << sep;
+      it = it->next;
+    } while (it != nullptr);
+    cout << endl;
   }
 
-  // Ponteiro para o fim da lista, que é a próxima posição livre da lista. (Corresponde a last->next)
+  // Agarra (uma cópia) do nodo da posição, pos, especificada. Retorna nullpointer se falhar
+  Node<Tipo> *get(int pos) {
+    if (pos >= size_) {
+      return nullptr;
+    }
+    Node<Tipo> *aux = first_;  // cicla por ponteiro para evitar atribuições desnecessárias.
+    for (int i = 0; i < pos; i++) {
+      aux = aux->next;
+    }
+    return aux;
+  }
+
+  // Retorna o valor do Tipo na posição, pos, especificada. Retorna um nodo totalmente nulo, se falhar
+  Tipo at(int pos) {
+    if (pos >= size_) {
+      Tipo nulinho;
+      return nulinho;
+    }
+    Node<Tipo> *aux = first_;  // cicla por ponteiro para evitar atribuições desnecessárias.
+    for (int i = 0; i < pos; i++) {
+      aux = aux->next;
+    }
+    return aux->key;
+  }
+  // Ponteiro para o fim da lista, que é a próxima posição livre da lista. (Corresponde a last_->next)
   Node<Tipo> *end() {
-    return last->next;
+    return last_->next;
   }
 };
 
--- TP7/src/main.cpp
+++ TP7/src/main.cpp
@@ -1,7 +1,7 @@
 #include <iomanip>
 #include <iostream>
-#include "lista.hpp"
 
+#include "lista.hpp"
 
 using namespace std;
 
@@ -10,8 +10,31 @@ int main() {
   l.push_back(1);
   l.push_back(2);
   l.push_back(3);
-  cout << l.size() << endl;
+  l.push_back(4);
+  l.push_back(5);
+  // l.push_front(0);
+
+  cout << endl;
+  l.list();
+  Lista<float> listFloat;
+  // t.push_front(l.get(2));
+  // t.push_front(l.get(0));
+  listFloat.push_front(0.898);
+  Node<double> doubleNode;
+  // doubleNode.key = listFloat.at(0);
+
+  doubleNode = *(l.get(0));
+
   cout << l.pop_front() << endl;
-  cout << l.pop_back() << endl;
+  auto it = l.get(1);
+  l.erase(it);
   cout << l.pop_front() << endl;
+  cout << "size: " << l.size() << endl;
+  l.list();
+  l.pop_back();
+  l.list();
 }
+/* l
+ 00034
+f 233
+t 0010233 */
