ce93b1529bae6b5ff141d1318d26169c7e2540d6
trevorbelmont <belmont.programmer@gmail.com>
2023-05-23 11:05:21 -0300

    Constata erro na explicação do slide

--- TP7/src/main.cpp
+++ TP7/src/main.cpp
@@ -9,24 +9,26 @@ using namespace std;
 
 int main() {
   int size = 10;
-  /*
-    int array[size];
-    int array2[size];
-    randomize(array, size, 20, -5);
-    list(array, size);
-    randomize(array2, size, 20, -5);
-    list(array2, size);
 
-   */
+  int array[size];
+  int array2[size];
+  randomize(array, size, 20, -5);
+  list(array, size);
+  randomize(array2, size, 20, -5);
+  list(array2, size);
 
-  char array[size] = {"ordenas"};
-  char array2[size] = {"ordenas"};
+  // char array[size] = {"ordenas"};
+  // char array2[size] = {"ordenas"};
   size = 7;
   cout << "Heapficando ambos: " << endl;
 
+  // Meu buildHeap
   buildHeap(array, size);
+
+  // Constrói heap segundo o slide
   ConstroiHeap(array2, size);
 
   list(array, size);
+  cout << "Constŕoi Heap pelo Slide (errado?!)" << endl;
   list(array2, size);
 }
